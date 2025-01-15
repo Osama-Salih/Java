7ed86dceac36e7ca192a60a0e2e067cde1761bc3
Osama Salih <osamasalih.ca@gmail.com>
2025-01-15 16:52:49 +0300

    While loop

--- src/App.java
+++ src/App.java
@@ -21,23 +21,56 @@ public class App {
         // int n2 = in.nextInt();
         // System.out.println(" n1 + n2 = " + (n1 + n2 ) );        
 
-        int num = 0;
-        System.out.println("Enter a number");
-        Scanner in = new Scanner(System.in);
-        num = in.nextInt();
+        // int num = 0;
+        // System.out.println("Enter a number");
+        // Scanner in = new Scanner(System.in);
+        // num = in.nextInt();
 
-        if (num % 2 == 0) 
-            if (num > 10) {
-                System.out.println("even");
-                System.out.println("and greater then 10");
-            } else {
-                System.out.println("even");
-                System.out.println("and less then 10");
-            }
+        // if (num % 2 == 0) 
+        //     if (num > 10) {
+        //         System.out.println("even");
+        //         System.out.println("and greater then 10");
+        //     } else {
+        //         System.out.println("even");
+        //         System.out.println("and less then 10");
+        //     }
 
-         else {
-                System.out.println("odd");
-            }
-        
+        //  else {
+        //         System.out.println("odd");
+        //     }
+
+        // int mark;
+        // Scanner in = new Scanner(System.in);
+
+        // System.out.println("Enter your mark: ");
+        // mark = in.nextInt();
+
+        // if (mark >= 90 && mark <= 100)
+        //     System.out.println("A");
+        // else if (mark >= 80)
+        //     System.out.println("B");    
+        // else if (mark >= 70)
+        //     System.out.println("C");
+        // else if (mark >= 60) 
+        //     System.out.println("D");
+        // else 
+        //     System.out.println("F");   
+       
+       Scanner in = new Scanner(System.in);
+
+       float sum = 0;
+       float grade = 0;
+       int count = 0;
+
+       while (grade != -1) {
+        System.out.println("Enter grade # " + (count + 1) + ": ");
+        grade = in.nextFloat();
+
+        if (grade != -1) {
+            sum += grade;
+            count++;
+        }
+       }
+       System.out.println("Avg = " + sum / count);
     }
 }
