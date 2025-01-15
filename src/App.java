6b4bab9e77a0b5faebd9eac2f4623a4f27b7550b
Osama Salih <osamasalih.ca@gmail.com>
2025-01-15 10:17:57 +0300

    Selection Statement ( if - if else - if else if )

--- src/App.java
+++ src/App.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
+    //    Scanner in = new Scanner(System.in);
     //    float n1, n2; 
        
     //    System.out.println("Please enter the first number: ");
@@ -15,10 +15,29 @@ public class App {
         //  String str = in.nextLine();
         //  System.out.printf("str = %S", str);
 
-        System.out.println("Enter num 1");
-        int n1 = in.nextInt(); 
-        System.out.println("Enter num 2");
-        int n2 = in.nextInt();
-        System.out.println(" n1 + n2 = " + (n1 + n2 ) );        
+        // System.out.println("Enter num 1");
+        // int n1 = in.nextInt(); 
+        // System.out.println("Enter num 2");
+        // int n2 = in.nextInt();
+        // System.out.println(" n1 + n2 = " + (n1 + n2 ) );        
+
+        int num = 0;
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        num = in.nextInt();
+
+        if (num % 2 == 0) 
+            if (num > 10) {
+                System.out.println("even");
+                System.out.println("and greater then 10");
+            } else {
+                System.out.println("even");
+                System.out.println("and less then 10");
+            }
+
+         else {
+                System.out.println("odd");
+            }
+        
     }
 }
